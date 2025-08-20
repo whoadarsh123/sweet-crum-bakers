162aae87b8b94b391367df62928dc493650707b0
adarsh tripathi <adarshtripathi262@gmail.com>
2025-08-20 12:50:01 +0530

    this is another commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ function Home()
         <div>
     <Items/>
     <div className='selling-items'>
-      <Card  Image="/images/rakhi-cake.png" title="Cakes" content="Upto 50% Off"/>
+      <Card  Image="/sweet-crum-bakers/rakhi-cake.png" title="Cakes" content="Upto 50% Off"/>
       <Card  Image="/images/snacks.png"  title="Namkeen" content="Upto 50% Off"/>
       <Card  Image="/images/sweets.png" title="Sweets" content="Upto 50% Off"/>
       <Card  Image="/images/pastries.png" title="Pastries" content="Upto 50% Off"/>
